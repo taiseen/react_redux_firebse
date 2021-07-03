d1c628ac33c412c2b890ca8ddf94b88ce4f78210
taiseen <taiseen.cse@gmail.com>
2021-07-03 15:02:11 +0600

    add date

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,39 +1,57 @@
+import moment from 'moment';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
 import { Redirect } from 'react-router-dom';
+import { compose } from 'redux';
 
 const ProjectDetails = (props) => {
-
     document.title = "Project Details";
 
-    //console.log(props)
-    const { userAuth } = props;
+    const { post, userAuth } = props;
+
     if (!userAuth.uid) return <Redirect to='/signin' />
 
-    const { content, title, authorFirstName, authorLastName } = props.location.state;
+    // const { content, title, authorFirstName, authorLastName, createdAt } = props.location.state;
 
-    // const id = props.match.params.id;
 
-    return (
-        <div className="container section project-details">
-            <div className="card z-depth-0">
-                <div className="card-content">
-                    <span className="card-title">{title} </span>
-                    <p>{content}</p>
-                </div>
+    if (post) {
+        
+        const { content, title, authorFirstName, authorLastName, createdAt } = post;
+        return (
+            <div className="container section project-details">
+                <div className="card z-depth-0">
+                    <div className="card-content">
+                        <span className="card-title">{title} </span>
+                        <p>{content}</p>
+                    </div>
 
-                <div className="card-action gret lighten-4 grey-text">
-                    <div>Posted by {authorFirstName} {authorLastName}</div>
-                    <div>2nd Jun, 2021</div>
+                    <div className="card-action gret lighten-4 grey-text">
+                        <div>Posted by {authorFirstName} {authorLastName}</div>
+                        <div>{moment(createdAt.toDate()).calendar()}</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div className="red-text center">Loading...</div>
+        );
+    }
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, idProps) => {
+
+    const id = idProps.match.params.id;
+    const posts = state.firestore.data.projects;
+    const post = posts ? posts[id] : null;
+
     return {
+        post: post,
         userAuth: state.firebaseAuth.auth
     }
 }
 
-export default connect(mapStateToProps)(ProjectDetails);
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([{ collection: 'projects' }])
+)(ProjectDetails);
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,13 +1,15 @@
+import moment from "moment";
+
 const ProjectSummary = ({ project }) => {
 
-    const { title, content , authorFirstName, authorLastName} = project;
+    const { title, content, authorFirstName, authorLastName, createdAt } = project;
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title">{title}</span>
                 <p className="mb-5">{content}</p>
                 <p>Posted by the <strong>{authorFirstName} {authorLastName}</strong></p>
-                <div className="grey-text">3rd September, 2am </div>
+                <div className="grey-text">{moment(createdAt.toDate()).calendar()}</div>
             </div>
         </div>
     );
